883be15ba3e28be8d8674e34f8099680fa63f7c6
ayushin <alexis@ww.net>
2021-07-05 12:17:15 +0000

    Deploying to gh-pages from @ apexlabs-ai/progress_builder@80ff4f51e104002d54bb4bdf9f372a146da0bba4 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,18 +5,18 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "b72098a9725236cb37524715efef353b",
+"index.html": "e009940badf54c79418e682004953169",
+"/": "e009940badf54c79418e682004953169",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
-"index.html": "1d61479c1891041344d5e11d69324ff0",
-"/": "1d61479c1891041344d5e11d69324ff0",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/AssetManifest.json": "4f9812c84afdcf5b4fffdddf1c7683c7",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/NOTICES": "b80442fba1759840d315e435f4bc8cfd",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/lib/main.dart": "f153418696f5dfd507e88718c2f20bed",
-"assets/NOTICES": "b80442fba1759840d315e435f4bc8cfd",
-"version.json": "4b6db237b3514a88107a422469adfb0f"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/AssetManifest.json": "4f9812c84afdcf5b4fffdddf1c7683c7",
+"version.json": "4b6db237b3514a88107a422469adfb0f",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
 
 // The application shell files that are downloaded before a service worker can
